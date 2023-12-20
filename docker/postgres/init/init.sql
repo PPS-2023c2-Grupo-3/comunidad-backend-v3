954dae3d991cf1fd08074d2a53975a4db1c5ca05
adrian yaniri <agyaniri@gmail.com>
2023-12-20 09:17:41 -0300

    conexion exitosa a la db

---
+++ docker/postgres/init/init.sql
@@ -0,0 +1,16 @@
+-- ./docker/postgres/init/init.sql
+-- Crear un usuario
+CREATE USER admin WITH PASSWORD 'admin2024' SUPERUSER;
+
+-- Crear la base de datos y asignar al usuario
+CREATE DATABASE empleabilidad WITH OWNER admin;
+
+-- Otorgar privilegios al usuario en la base de datos
+GRANT ALL PRIVILEGES ON DATABASE empleabilidad TO admin;
+
+-- Otorgar privilegios específicos en las tablas (ejemplo)
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO admin;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO admin;
+
+-- Otorgar privilegios de conexión (si es necesario)
+ALTER USER admin WITH CONNECTION LIMIT -1;
